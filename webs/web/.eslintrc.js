22547217705a45f38dc041cc2e2e98b1ffa4959f
常兆海 <1341195069@qq.com>
2023-06-11 19:48:18 +0800

    全屏组件的引入

--- webs/web/.eslintrc.js
+++ webs/web/.eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     'vue/no-unused-components':'off',
     "vue/no-unused-vars": 0,
     "@typescript-eslint/no-explicit-any": "off",
-  }
+  },
+
 }
--- webs/web/src/views/home.vue
+++ webs/web/src/views/home.vue
@@ -1,55 +1,57 @@
 <template>
 
-  <a-layout-content style="padding: 0 50px">
+    <a-layout-content style="padding: 0 50px">
 
-    <a-layout style="padding: 24px 0; background: #fff">
-        <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
-            
-            
-        <div class="welcome" v-show="isshowwelcome">
-         <TheView></TheView>
-        </div>
-        </div>
-    </a-layout>
-  </a-layout-content>
+        <a-layout style="padding: 24px 0; background: #fff">
+
+          <div>
+
+              <a-button @click="fullScreen">全屏</a-button>
+
+            <div  id="view"></div>
+
+
+
+                <TheView></TheView>
+          </div>
+
+
+
+        </a-layout>
+    </a-layout-content>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, ref} from 'vue';
+import {defineComponent, onMounted, ref, onUnmounted, reactive} from 'vue';
 import axios from "axios";
-import {UserOutlined, HomeOutlined,FireOutlined,FileTextOutlined} from '@ant-design/icons-vue';
+import {UserOutlined, HomeOutlined, FireOutlined, FileTextOutlined} from '@ant-design/icons-vue';
 import {Tool} from "@/utils/tool";
 import {message} from "ant-design-vue";
 import TheView from "@/components/the-view.vue";
+import screenfull from "screenfull";
 
 export default defineComponent({
-  name: 'Home',
-
+  name: 'home',
   components: {
-    TheView,
+    TheView
   },
   setup() {
-    console.log("setup")
-    const isshowwelcome = ref(true);
-    onMounted(() => {//生命周期函数
-    });
-    return {
-      // pagination,
-      isshowwelcome,  //互斥方法显示是否显示欢迎页面
-
-    }
+    const fullScreen = () => {
+    message.info('全屏');
+         if (screenfull.isEnabled && !screenfull.isFullscreen) {
+           screenfull.request();
+         }
   }
-});
+  return {
+    fullScreen,
+  }
+  }
+}) ;
+
+
+
 </script>
-<style scoped>
-/*scoped 作用  限制样式只在当前组件生效*/
-.ant-avatar {
-  width: 50px;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 8%;
-  margin: 5px 0;
-}</style>
 <style>
+
 
 </style>
