c7fb2c876535618bba616a1b8e2e40bce7cc5f29
常兆海 <1341195069@qq.com>
2023-06-16 14:40:55 +0800

    前端温度湿度检测器无启动的初始化显示

--- webs/web/src/components/the-marquee.vue
+++ webs/web/src/components/the-marquee.vue
@@ -1,73 +1,95 @@
 <template style="width: 100%;height:248px;">
 
-    <a-carousel autoplay>
-      <div   class="card1" style="width: 100%;height:248px;"  v-for="item in templists" :key="item.id">
-        <a-card>
-          <h1>当前设备为：{{item.name}}</h1>
-          <h3>位置：{{item.location}}</h3>
-          <h2    class="aaa" v-if="item.classes&&item.temperatures>24 " >当前温度为：{{item.temperatures}}℃</h2>
-          <h2 v-else-if="item.classes&&item.temperatures<=24"  >当前温度为：{{item.temperatures}}℃</h2>
+  <a-carousel autoplay>
+    <div class="card1" style="width: 100%;height:248px;" v-for="item in templists" :key="item.id">
+
+      <a-card>
+
+        <h1>当前设备为：{{ item.name }}</h1>
+        <h3>位置：{{ item.location }}</h3>
+        <h2 class="aaa" v-if="item.classes&&item.temperatures>28 ">当前温度为：{{ item.temperatures }}℃</h2>
+        <h2 v-else-if="item.classes&&item.temperatures<=28">当前温度为：{{ item.temperatures }}℃</h2>
 
 
-          <h2 class="aaa"  v-else-if="!item.classes&&item.temperatures>70 " >当前湿度为：{{item.temperatures}}%</h2>
-          <h2 v-else>当前湿度为：{{item.temperatures}}%</h2>
+        <h2 class="aaa" v-else-if="!item.classes&&item.temperatures>70 ">当前湿度为：{{ item.temperatures }}%</h2>
+        <h2 v-else>当前湿度为：{{ item.temperatures }}%</h2>
 
 
-        </a-card>
+      </a-card>
 
-      </div>
+    </div>
 
-    </a-carousel>
-<!--  </template>-->
+  </a-carousel>
+  <!--  </template>-->
 
-<!--  <a-carousel arrows>-->
-<!--    <template #prevArrow>-->
-<!--      <div class="custom-slick-arrow" style="left: 10px; z-index: 1">-->
-<!--        <left-circle-outlined />-->
-<!--      </div>-->
-<!--    </template>-->
-<!--    <template #nextArrow>-->
-<!--      <div class="custom-slick-arrow" style="right: 10px" >-->
-<!--        <right-circle-outlined />-->
-<!--      </div>-->
-<!--    </template>-->
+  <!--  <a-carousel arrows>-->
+  <!--    <template #prevArrow>-->
+  <!--      <div class="custom-slick-arrow" style="left: 10px; z-index: 1">-->
+  <!--        <left-circle-outlined />-->
+  <!--      </div>-->
+  <!--    </template>-->
+  <!--    <template #nextArrow>-->
+  <!--      <div class="custom-slick-arrow" style="right: 10px" >-->
+  <!--        <right-circle-outlined />-->
+  <!--      </div>-->
+  <!--    </template>-->
 
-<!-- -->
-<!--  </a-carousel>-->
+  <!-- -->
+  <!--  </a-carousel>-->
 </template>
 <script lang="ts">
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons-vue';
+import {LeftCircleOutlined, RightCircleOutlined} from '@ant-design/icons-vue';
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
+
 declare let echarts: any;
+import {Tool} from "@/utils/tool";
+
 export default defineComponent({
+  computed: {
+    Tool() {
+      return Tool
+    }
+  },
   components: {
     LeftCircleOutlined,
     RightCircleOutlined,
   },
-  setup (){
+  setup() {
 
     //============================================================================================================================================================
-    const aa=ref();
+    const aa = ref();
     const a = () => {
       axios.get("/a/a").then((res) => {
         // console.log(res.data.content,"aaaaa");
-        aa.value=res.data.content;
+        aa.value = res.data.content;
       });
 
     };
-    const templists=ref();
-    const  tempall=()=>{
-      axios.get("/equipment/tempall").then((res)=>{
+    const templists = ref([
+      {
+        id: "404",
+        name: '无检测器启动',
+        location: "无",
+        classes: "",
+        temperatures: "",
+      },
+    ])//给他赋初值
+
+
+    const tempall = () => {
+      axios.get("/equipment/tempall").then((res) => {
+        console.log(Tool.isEmpty(templists.value), "              对比")
         const data = res.data;
-        if (data.success) {
-         // console.log(data.content,"dataaaaa")
-          templists.value=res.data.content;
+        if (data.success && !Tool.isEmpty(templists.value)) {
+          // console.log(data.content,"dataaaaa")
+          templists.value = res.data.content;
         }
+        console.log(templists, "11111111````````````````````````````````````")
       })
     }
     onMounted(() => {
-     tempall()
+      tempall()
       a();
     });
     return {
@@ -92,6 +114,7 @@ export default defineComponent({
 .ant-carousel :deep(.slick-slide div) {
   color: #1185cc;
 }
+
 .ant-carousel :deep(.slick-slide .aaa) {
   color: #dc0000;
 }
@@ -99,7 +122,7 @@ export default defineComponent({
 </style>
 
 <style lang="less" scoped>
-.card1  {
+.card1 {
   :deep( .ant-card ) {
     background: rgba(255, 255, 255, 0.08);
   }
